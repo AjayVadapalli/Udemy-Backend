35a8ce30a44adfc39e06a3c5abc020b35fc76e91
“AjayVadapalli” <vajaysivasurya@gmail.com>
2025-02-11 21:27:35 +0530

    flg_project

--- 8.2 Postgres Read/index.js
+++ 8.2 Postgres Read/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user:"postgres",
+  password:"#Solo143",
+  database:"World",
+  host:"localhost",
+  port:5432,
+});
+db.connect();
 
 let totalCorrect = 0;
 
@@ -11,6 +21,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentQuestion = {};
+
+let quiz = [
+
+];
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 // GET home page
 app.get("/", (req, res) => {
@@ -24,7 +46,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion?.capital?.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
